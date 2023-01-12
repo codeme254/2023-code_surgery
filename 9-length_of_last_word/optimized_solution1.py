09260b0e65e08d42c7816a36f9d5c46663d0dc1f
codeme254 <otwomadennis365@gmail.com>
2023-01-12 03:31:25 -0500

    Length of last word

---
+++ 9-length_of_last_word/optimized_solution1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        s_arr = s.split()
+        return len(s_arr[len(s_arr) - 1])
+
+solution = Solution()
+print(solution.lengthOfLastWord("  Fly Me to the Moon    "))
+#!/usr/bin/env python3
