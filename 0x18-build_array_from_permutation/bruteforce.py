4f6b1f000b80be8b4f5d7afa6f7fcd8de681ee0a
codeme254 <otwomadennis365@gmail.com>
2023-02-04 06:37:44 -0500

    Build array from permutation

--- 0x18-build_array_from_permutation/bruteforce.py
+++ 0x18-build_array_from_permutation/bruteforce.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        answer_array = []
+
+        for i in range(0, len(nums)):
+            answer_array.append(nums[nums[i]])
+        
+        return answer_array
+    
+solution = Solution()
+print(solution.buildArray([0,2,1,5,3,4]))
